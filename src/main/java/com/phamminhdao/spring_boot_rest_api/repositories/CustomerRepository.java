c7c24d1f55eba3740f21559d91048e9d630bcc6e
Dao Pham <daopm.dean@gmail.com>
2021-10-09 10:34:53 +0700

    feat: customer repository implements with hibernate session

--- src/main/java/com/phamminhdao/spring_boot_rest_api/repositories/CustomerRepository.java
+++ src/main/java/com/phamminhdao/spring_boot_rest_api/repositories/CustomerRepository.java
@@ -6,4 +6,7 @@ import java.util.List;
 
 public interface CustomerRepository {
     public List<Customer> findAll();
+    public Customer findById(int id);
+    public void save(Customer customer);
+    public void deleteById(int id);
 }
--- src/main/java/com/phamminhdao/spring_boot_rest_api/repositories/impls/CustomerRepositoryImpl.java
+++ src/main/java/com/phamminhdao/spring_boot_rest_api/repositories/impls/CustomerRepositoryImpl.java
@@ -3,12 +3,12 @@ package com.phamminhdao.spring_boot_rest_api.repositories.impls;
 import com.phamminhdao.spring_boot_rest_api.entities.Customer;
 import com.phamminhdao.spring_boot_rest_api.repositories.CustomerRepository;
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
-import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
@@ -21,15 +21,41 @@ public class CustomerRepositoryImpl implements CustomerRepository {
     }
 
     @Override
-    @Transactional
     public List<Customer> findAll() {
         Session currentSession = entityManager.unwrap(Session.class);
 
-        Query theQuery =
-                currentSession.createQuery("from customers", Customer.class);
+        Query<Customer> theQuery =
+                currentSession.createQuery("from Customer", Customer.class);
 
         List<Customer> customers = theQuery.getResultList();
 
         return customers;
+    }
+
+    @Override
+    public Customer findById(int id) {
+        Session currentSession = entityManager.unwrap(Session.class);
+
+        Customer customer = currentSession.get(Customer.class, id);
+
+        return customer;
+    }
+
+    @Override
+    public void save(Customer customer) {
+        Session currentSession = entityManager.unwrap(Session.class);
+
+        currentSession.saveOrUpdate(customer);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        Session currentSession = entityManager.unwrap(Session.class);
+
+        Query query = currentSession
+                .createQuery("delete from Customer where id=:id");
+        query.setParameter("id", id);
+
+        query.executeUpdate();
     }
 }
